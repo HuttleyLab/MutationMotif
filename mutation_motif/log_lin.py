e748282e5281a7809a4b74ef34d0a64d02f4d4bc
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2015-02-12 09:04:58 +1100

    mutation_analysis now executes log-linear modelling and produces plots
    [NEW] added log_lin.py module. This relies on rpy2 for doing the log-linear
        modelling. Currently limited to 4-way interactions.
    [CHANGED] command line arg for alignment is now -a or --alignfile

---
+++ mutation_motif/log_lin.py
@@ -0,0 +1,94 @@
+from rpy2.robjects import Formula, r as R
+from rpy2.robjects.vectors import DataFrame, StrVector, IntVector, FactorVector
+
+import pandas as pd
+
+import numpy
+
+def as_dataframe(table):
+    '''returns a DataFrame instance. Requires counts to be [[col1, col2, col3, ..]]'''
+    data = dict(zip(table.Header, zip(*table.getRawData())))
+    for column in data:
+        if type(data[column][0]) in (unicode, str):
+            klass = StrVector
+        else:
+            klass = IntVector
+        
+        data[column] = klass(data[column])
+        
+    return DataFrame(data)
+
+def get_rpy2_factorvector_index_map(fac):
+    '''returns dict mapping factor vector indices to factors'''
+    mapping = dict([(i+1, f) for i, f in enumerate(fac.levels)])
+    return mapping
+
+def convert_rdf_to_pandasdf(r_df):
+    '''converts an rpy2 dataframe to a pandas dataframe'''
+    converted = {}
+    for col_name, col_vector in r_df.items():
+        if type(col_vector) == FactorVector:
+            index_factor_map = get_rpy2_factorvector_index_map(col_vector)
+            col_vector = [index_factor_map[val] for val in col_vector]
+        converted[col_name] = list(col_vector)
+    return pd.DataFrame(converted)
+
+def DevianceToRelativeEntropy(N):
+    '''converts deviance to Relative Entropy'''
+    denom = 2 * N
+    def call(val):
+        return val / denom
+    return call
+
+def CalcRet(dev_to_re, epsilon=1e-9):
+    '''factory function for computing residual relative entropy terms.
+    
+    dev_to_re is a function for converting a deviance to relative entropy'''
+    def call(obs, exp):
+        result = []
+        for i in range(len(obs)):
+            o, e = obs[i], exp[i]
+            e = e or epsilon # to avoide zero division
+            ret = dev_to_re(2 * o * numpy.log(o / e))
+            result.append(ret)
+        return result
+    return call
+
+def position_effect(counts_table, test=False):
+    """returns total relative entropy, degrees of freedom and stats
+    
+    fit's a log-lin model that is excludes only the full interaction term"""
+    num_pos = len(counts_table.Header) - 2
+    assert 1 <= num_pos <= 4, "Can only handle 4 positions"
+    
+    if num_pos == 1:
+        columns = ['mut', 'base', 'count']
+    else:
+        columns = ['mut'] + ['base%d' % (i+1) for i in range(num_pos)] + ['count']
+    
+    factors = columns[:-1]
+    formula = " - ".join([" * ".join(factors), " : ".join(factors)])
+    formula = "count ~ %s" % formula
+    null = Formula(formula)
+    if test:
+        print formula
+    
+    counts_table = counts_table.getColumns(columns)
+    d = as_dataframe(counts_table)
+    
+    f = R.glm(null, data=d, family = "poisson")
+    f_attr = dict(f.items())
+    dev = f_attr['deviance'][0]
+    df = f_attr['df.null'][0]
+    
+    collated = convert_rdf_to_pandasdf(f_attr['data'])
+    collated['fitted'] = list(f_attr['fitted.values'])
+    dev_to_re = DevianceToRelativeEntropy(collated['count'].sum())
+    calc_ret = CalcRet(dev_to_re)
+    total_re = dev_to_re(dev)
+    
+    collated['ret'] = calc_ret(collated['count'], collated['fitted'])
+    collated = collated.reindex_axis(columns + ['fitted', 'ret'], axis=1)
+    collated = collated.sort(columns=columns[:-1])
+    return total_re, dev, df, collated
+    
--- mutation_motif/mutation_analysis.py
+++ mutation_motif/mutation_analysis.py
@@ -1,16 +1,298 @@
 #!/usr/bin/env python
 import os, glob, re
-from itertools import permutations
+from itertools import permutations, combinations
 from optparse import make_option
 
 import numpy
+from matplotlib import pyplot
 
 from cogent import LoadSeqs, DNA, LoadTable
 from cogent.core.alignment import DenseAlignment
 from cogent.util.option_parsing import parse_command_line_parameters
 
-from mutation_motif import profile, util, height, logo, entropy, distribution,\
-            motif_count
+from mutation_motif import profile, util, logo, motif_count
+
+from mutation_motif.height import get_re_char_heights
+from mutation_motif import logo
+
+import motif_count, log_lin
+
+def single_position_effects(table, positions):
+    single_results = {}
+    for position in positions:
+        counts = motif_count.get_combined_counts(table, position)
+        rel_entropy, deviance, df, stats = log_lin.position_effect(counts)
+        single_results[position] = dict(rel_entropy=rel_entropy, deviance=deviance, df=df, stats=stats)
+    return single_results
+
+def get_single_position_fig(single_results, positions, figsize, fig_width=None, fontsize=14):
+    num_pos = len(positions) + 1
+    mid = num_pos // 2
+
+    position_re = numpy.zeros((num_pos,), float)
+    rets = numpy.zeros((4, num_pos), float)
+    characters = [list('ACGT') for i in range(num_pos)]
+    for index, pos in enumerate(positions):
+        if index >= mid:
+            index += 1
+
+        stats = single_results[pos]['stats']
+        position_re[index] = single_results[pos]['rel_entropy']
+        mut_stats = stats[stats['mut'] == 'M'][['base', 'ret']]
+        mut_stats = mut_stats.sort('ret')
+        characters[index] = list(mut_stats['base'])
+        rets[:,index] = mut_stats['ret']
+
+    heights = get_re_char_heights(rets, re_positionwise=position_re)
+    fig = logo.draw_multi_position(heights.T, characters=characters, position_indices=range(num_pos),
+                               figsize=figsize, fig_width=fig_width, verbose=False)
+    
+    if fig_width:
+        fig.set_figwidth(fig_width)
+    
+    ax = fig.gca()
+    ax.set_xlabel('Position', fontsize=fontsize)
+    ax.set_ylabel('RE', rotation='vertical', fontsize=fontsize)
+    return fig
+
+def get_resized_array_coordinates2(positions, motifs):
+    '''coords for the position combinations for the smaller array used for plotting'''
+    # to avoid blank rows/columns in the trellis plot, we reduce the array size
+    num_pos = len(positions)
+    a = numpy.zeros((num_pos, num_pos), object)
+    for motif in motifs:
+        indices = map(positions.index, motif)
+        indices.reverse()
+        a[indices[0]][indices[1]] = motif
+    
+    new = a[(a != 0).any(axis=1)]
+    # it's a 2D array now
+    new = new[:, (new != 0).any(axis=0)]
+    mapping = {}
+    for i in range(new.shape[0]):
+        for j in range(new.shape[1]):
+            if new[i, j] != 0:
+                mapping[new[i, j]] = (i, j)
+    return mapping
+
+def get_two_position_effects(table, positions):
+    two_pos_results = {}
+    rel_entropies = []
+    position_sets = list(combinations(positions, 2))
+    for position_set in position_sets:
+        counts = motif_count.get_combined_counts(table, position_set)
+        rel_entropy, deviance, df, stats = log_lin.position_effect(counts)
+        two_pos_results[position_set] = dict(rel_entropy=rel_entropy, deviance=deviance, df=df, stats=stats)
+        rel_entropies.append(rel_entropy)
+    return two_pos_results
+
+def get_two_position_fig(two_pos_results, positions, figsize, fig_width=None, fontsize=14):
+    position_sets = list(combinations(positions, 2))
+    array_coords = get_resized_array_coordinates2(positions, position_sets)
+    coords = array_coords.values()
+    xdim = max(v[0] for v in coords) + 1
+    ydim = max(v[1] for v in coords) + 1
+    fig, axarr = pyplot.subplots(xdim, ydim, figsize=figsize, sharex=True, sharey=True)
+
+    for i in range(xdim-1):
+        for j in range(i+1, ydim):
+            ax = axarr[i,j]
+            ax.set_frame_on(False)
+            ax.get_xaxis().set_visible(False)
+            ax.get_yaxis().set_visible(False)
+
+    rel_entropies = []
+    for position_set in position_sets:
+        rel_entropies.append(two_pos_results[position_set]['rel_entropy'])
+    ylim = logo.est_ylim(rel_entropies)
+    
+    num_pos = len(positions) + 1
+
+    mid_pos = num_pos // 2
+
+    position_re = numpy.zeros((num_pos,), float)
+    multi_positions = {}
+    characters = numpy.zeros((num_pos,  16), str)
+
+    for pair in position_sets:
+        rets = numpy.zeros((16, num_pos), float)
+        indices = map(positions.index, pair)
+        row, col = array_coords[pair]
+        ax = axarr[row, col]
+
+        # now adjust indices to reflect position along sequence
+        for i in range(2):
+            if indices[i] >= mid_pos:
+                indices[i] += 1
+
+        position_re.put(indices, two_pos_results[pair]['rel_entropy'])
+
+        stats = two_pos_results[pair]['stats']
+        mut_stats = stats[stats['mut'] == 'M'][['base1', 'base2', 'ret']]
+        mut_stats = mut_stats.sort(columns='ret')
+
+        characters[indices[0]] = list(mut_stats['base1'])
+        characters[indices[1]] = list(mut_stats['base2'])
+
+        for index in indices:
+            rets[:, index] = mut_stats['ret']
+
+        heights = get_re_char_heights(rets, re_positionwise=position_re)
+        multi_positions[pair] = dict(rets=rets, indices=indices, characters=characters, heights=heights)
+        logo.draw_multi_position(char_heights=heights.T, characters=characters,
+                                 position_indices=indices, ax=ax, ylim=ylim)
+    return fig
+
+def get_resized_array_coordinates3(positions, position_set):
+    '''coords for the position combinations for the smaller array used for plotting'''
+    # to avoid blank rows/columns in the trellis plot, we reduce the array size
+    num_pos = len(positions)
+    a = numpy.zeros((num_pos, num_pos, num_pos), object)
+    for triple in position_set:
+        indices = map(positions.index, triple)
+        indices.reverse()
+        a[indices[0]][indices[1]][indices[2]] = triple
+    
+    new = a.flatten()
+    new = new[new != 0]
+    new.sort()
+    new = new.reshape((2,2))
+    mapping = {}
+    for i in range(new.shape[0]):
+        for j in range(new.shape[1]):
+            if new[i, j] != 0:
+                mapping[new[i, j]] = (i, j)
+    return mapping
+
+def get_three_position_effects(table, positions):
+    three_pos_results = {}
+    position_sets = list(combinations(positions, 3))
+    for position_set in position_sets:
+        counts = motif_count.get_combined_counts(table, position_set)
+        rel_entropy, deviance, df, stats = log_lin.position_effect(counts)
+        three_pos_results[position_set] = dict(rel_entropy=rel_entropy, deviance=deviance, df=df, stats=stats)
+    return three_pos_results
+
+def get_three_position_fig(three_pos_results, positions, figsize, fig_width=None, fontsize=14):
+    position_sets = list(combinations(positions, 3))
+    array_coords = get_resized_array_coordinates3(positions, position_sets)
+
+    coords = array_coords.values()
+    xdim = max(v[0] for v in coords) + 1
+    ydim = max(v[1] for v in coords) + 1
+
+    fig, axarr = pyplot.subplots(xdim, ydim, figsize=(9,9), sharex=True, sharey=True)
+
+    for i in range(xdim):
+        for j in range(ydim):
+            if (i,j) in coords:
+                continue
+
+            ax = axarr[i,j]
+            ax.set_frame_on(False)
+            ax.get_xaxis().set_visible(False)
+            ax.get_yaxis().set_visible(False)
+
+
+    num_pos = len(positions) + 1
+    mid_pos = num_pos // 2
+    
+    rel_entropies = []
+    for position_set in position_sets:
+        rel_entropies.append(three_pos_results[position_set]['rel_entropy'])
+    ylim = logo.est_ylim(rel_entropies)
+
+
+
+    position_re = numpy.zeros((num_pos,), float)
+    multi_positions = {}
+    characters = numpy.zeros((num_pos, 64), str)
+
+    for motif in combinations(positions, 3):
+        rets = numpy.zeros((64, num_pos), float)
+        indices = map(positions.index, motif)
+        row, col = array_coords[motif]
+        ax = axarr[row, col]
+
+        # now adjust indices to reflect position along sequence
+        for i in range(len(indices)):
+            if indices[i] >= mid_pos:
+                indices[i] += 1
+
+        position_re.put(indices, three_pos_results[motif]['rel_entropy'])
+
+        stats = three_pos_results[motif]['stats']
+        mut_stats = stats[stats['mut'] == 'M'][['base1', 'base2', 'base3', 'ret']]
+        mut_stats = mut_stats.sort(columns='ret')
+
+        characters[indices[0]] = list(mut_stats['base1'])
+        characters[indices[1]] = list(mut_stats['base2'])
+        characters[indices[2]] = list(mut_stats['base3'])
+
+
+        for index in indices:
+            rets[:, index] = mut_stats['ret']
+
+        heights = get_re_char_heights(rets, re_positionwise=position_re)
+        multi_positions[motif] = dict(rets=rets, indices=indices, characters=characters, heights=heights)
+        logo.draw_multi_position(char_heights=heights.T, characters=characters,
+                                 position_indices=indices, ax=ax, ylim=ylim)
+
+    return fig
+
+def get_four_position_effects(table, positions):
+    result = {}
+    position_sets = list(combinations(positions, 4))
+    for position_set in position_sets:
+        counts = motif_count.get_combined_counts(table, position_set)
+        rel_entropy, deviance, df, stats = log_lin.position_effect(counts)
+        result[position_set] = dict(rel_entropy=rel_entropy, deviance=deviance, df=df, stats=stats)
+    return result
+
+def get_four_position_fig(four_pos_results, positions, figsize, fig_width=None, fontsize=14):
+    position_sets = list(combinations(positions, 4))
+    assert len(position_sets) == 1
+    rel_entropies = [four_pos_results[position_sets[0]]['rel_entropy']]
+    ylim = logo.est_ylim(rel_entropies)
+    
+    rel_entropy = rel_entropies[0]
+    
+    fig = pyplot.figure(figsize=(9,9))
+    ax = fig.gca()
+
+    num_pos = len(positions) + 1
+
+    mid_pos = num_pos // 2
+
+    position_re = numpy.zeros((num_pos,), float)
+    characters = numpy.zeros((num_pos, 256), str)
+
+    rets = numpy.zeros((256, num_pos), float)
+    indices = range(4)
+
+    # now adjust indices to reflect position along sequence
+    for i in range(len(indices)):
+        if indices[i] >= mid_pos:
+            indices[i] += 1
+
+    position_re.put(indices, rel_entropy)
+    stats = four_pos_results[position_sets[0]]['stats']
+    mut_stats = stats[stats['mut'] == 'M'][['base1', 'base2', 'base3', 'base4', 'ret']]
+    mut_stats = mut_stats.sort(columns='ret')
+
+    characters[indices[0]] = list(mut_stats['base1'])
+    characters[indices[1]] = list(mut_stats['base2'])
+    characters[indices[2]] = list(mut_stats['base3'])
+    characters[indices[3]] = list(mut_stats['base4'])
+
+    for index in indices:
+        rets[:, index] = mut_stats['ret']
+
+    heights = get_re_char_heights(rets, re_positionwise=position_re)
+    logo.draw_multi_position(char_heights=heights.T, characters=characters,
+                             position_indices=indices, ax=ax, ylim=ylim)
+
+    return fig
 
 script_info = {}
 script_info['brief_description'] = ""
@@ -20,7 +302,7 @@ script_info['script_description'] = \
 + "directory outpath."
 
 script_info['required_options'] = [
-     make_option('-s','--alignfile', help='fasta aligned file centred on mutated position.'),
+     make_option('-a','--alignfile', help='fasta aligned file centred on mutated position.'),
      make_option('-o','--outpath', help='Directory path to write data.'),
     make_option('--direction', default=None,
      choices=['AtoC', 'AtoG', 'AtoT', 'CtoA', 'CtoG', 'CtoT', 'GtoA', 'GtoC',
@@ -53,28 +335,101 @@ def main():
     if not opts.dry_run:
         util.create_path(outpath)
     
-    direction = tuple(opts.direction.split('to'))
-    chosen_base = direction[0]
+    counts_filename = os.path.join(outpath, "counts_table.txt")
     
-    fn = util.abspath(opts.alignfile)
-    orig_seqs = util.load_from_fasta(fn)
-    seqs = orig_seqs.ArraySeqs
-    seqs = util.just_nucs(seqs)
-    orig, ctl = profile.get_profiles(seqs, chosen_base=chosen_base, step=1,
-                                    flank_size=opts.flank_size)
+    print "Deriving counts from sequence file"
+    if not os.path.exists(counts_filename):
+        direction = tuple(opts.direction.split('to'))
+        chosen_base = direction[0]
     
-    # convert profiles to a motif count table
-    orig_counts = motif_count.profile_to_seq_counts(orig, flank_size=opts.flank_size)
-    ctl_counts = motif_count.profile_to_seq_counts(ctl, flank_size=opts.flank_size)
-    counts_table = motif_count.get_count_table(orig_counts, ctl_counts, opts.flank_size*2)
+        fn = util.abspath(opts.alignfile)
+        orig_seqs = util.load_from_fasta(fn)
+        seqs = orig_seqs.ArraySeqs
+        seqs = util.just_nucs(seqs)
+        orig, ctl = profile.get_profiles(seqs, chosen_base=chosen_base, step=1,
+                                        flank_size=opts.flank_size)
     
-    if not opts.dry_run:
-        outpath = os.path.join(outpath, "counts_table.txt")
-        counts_table.writeToFile(outpath, sep='\t')
+        # convert profiles to a motif count table
+        orig_counts = motif_count.profile_to_seq_counts(orig, flank_size=opts.flank_size)
+        ctl_counts = motif_count.profile_to_seq_counts(ctl, flank_size=opts.flank_size)
+        counts_table = motif_count.get_count_table(orig_counts, ctl_counts, opts.flank_size*2)
+        counts_table = counts_table.sorted(columns='mut')
+        if not opts.dry_run:
+            counts_table.writeToFile(counts_filename, sep='\t')
+    else:
+        counts_table = LoadTable(counts_filename, sep='\t')
+    
     
     if opts.dry_run or opts.verbose:
         print
         print counts_table
         print
     
+    positions = [c for c in counts_table.Header if c.startswith('pos')]
     
+    max_results = {}
+    # Single position analysis
+    print "Doing single position analysis"
+    single_results = single_position_effects(counts_table, positions)
+    max_results[1] = max(single_results[p]['rel_entropy'] for p in single_results)
+    fig = get_single_position_fig(single_results, positions, (9,3), fig_width=2.25)
+    fig.tight_layout()
+    if not opts.dry_run:
+        outfilename = os.path.join(outpath, "1.pdf")
+        fig.savefig(outfilename)
+        print "Wrote", outfilename
+        fig.clf() # refresh for next section
+    
+    print "Doing two positions analysis"
+    results = get_two_position_effects(counts_table, positions)
+    max_results[2] = max(results[p]['rel_entropy'] for p in results)
+    fig = get_two_position_fig(results, positions, (9,9))
+    fig.set_figwidth(9)
+    fig.text(0.5, 0.05, 'Position', ha='center', va='center', fontsize=14)
+    fig.text(0.01, 0.5, 'RE', ha='center', va='center', rotation='vertical', fontsize=14)
+    if not opts.dry_run:
+        outfilename = os.path.join(outpath, "2.pdf")
+        fig.savefig(outfilename)
+        print "Wrote", outfilename
+        fig.clf() # refresh for next section
+    
+    print "Doing three positions analysis"
+    results = get_three_position_effects(counts_table, positions)
+    max_results[3] = max(results[p]['rel_entropy'] for p in results)
+    fig = get_three_position_fig(results, positions, (9,9))
+    fig.set_figwidth(9)
+    fig.text(0.5, 0.05, 'Position', ha='center', va='center', fontsize=14)
+    fig.text(0.01, 0.5, 'RE', ha='center', va='center', rotation='vertical', fontsize=14)
+    if not opts.dry_run:
+        outfilename = os.path.join(outpath, "3.pdf")
+        fig.savefig(outfilename)
+        print "Wrote", outfilename
+        fig.clf() # refresh for next section
+    
+    print "Doing four positions analysis"
+    results = get_four_position_effects(counts_table, positions)
+    max_results[4] = max(results[p]['rel_entropy'] for p in results)
+    fig = get_four_position_fig(results, positions, (9,9))
+    fig.set_figwidth(9)
+    ax = fig.gca()
+    ax.set_xlabel('Position', fontsize=14)
+    ax.set_ylabel('RE', fontsize=14)
+    if not opts.dry_run:
+        outfilename = os.path.join(outpath, "4.pdf")
+        fig.savefig(outfilename)
+        print "Wrote", outfilename
+        fig.clf() # refresh for next section
+    
+    # now generate summary plot
+    bar_width = 0.5
+    index = numpy.arange(4)
+    bar = pyplot.bar(index, [max_results[i] for i in range(1,5)], bar_width)
+    pyplot.xticks(index+(bar_width/2.), range(1,5))
+    pyplot.ylabel("RE$_{max}$")
+    pyplot.xlabel("Motif Length")
+    if not opts.dry_run:
+        outfilename = os.path.join(outpath, "summary.pdf")
+        pyplot.savefig(outfilename)
+        print "Wrote", outfilename
+    
+    print "Done! Check %s for your results" % outpath
