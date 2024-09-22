9d631db6182d64024474a71f44a1336d93ec1ac3
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2024-09-22 12:12:39 +1000

    REL: bumpy version to 2024.9.22

--- src/mutation_motif/__init__.py
+++ src/mutation_motif/__init__.py
@@ -1,4 +1,4 @@
-"""MutationMotif, see https://www.ncbi.nlm.nih.gov/pubmed/27974498"""
+"""mutation_motif, software for naalyses of point mutations, see https://www.ncbi.nlm.nih.gov/pubmed/27974498"""
 
 from warnings import filterwarnings
 
@@ -6,4 +6,4 @@ filterwarnings("ignore", "Attempting to set identical bottom==top")
 filterwarnings("ignore", "invalid value encountered.*")
 
 __license__ = "BSD-3"
-__version__ = "2023.4.18"
+__version__ = "2024.9.22"
